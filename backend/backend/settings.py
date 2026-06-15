3666da8142a5457c585ac83fa961a815564aae45
Heaven-Bro <heavenchakma04@gmail.com>
2026-06-15 21:36:45 +0600

    Fix typos in settings.py and update token URL patterns in urls.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
 
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowedAny'
+        'rest_framework.permissions.AllowAny'
     ),
 }
 
@@ -155,6 +155,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS':False,
-    'BLACKLIST_AFTER_ ROTATION': True,
-    'AUTH_HEADER_TYPES':{'Bearer,'},
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES':('Bearer,'),
 }
--- backend/store/urls.py
+++ backend/store/urls.py
@@ -4,8 +4,8 @@ from  rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshVie
 
 urlpatterns = [
     path('register', views.register_view),
-    path('token', TokenObtainPairView.as_views(), name = 'token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_views(), name = 'token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('products/', views.get_products),
     path('products/<int:pk>/', views.get_product_detail),
     path('categories/', views.get_categories),
